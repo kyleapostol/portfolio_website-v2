06db4304b2d6ee2eb0c28775ec2b618e186a9ba2
Kyle Apostol <kyleapostol@yahoo.com>
2021-06-17 21:14:03 -0700

    completed portfolio

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,19 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+<Section row nopadding>
+  <LeftSection>
+    <SectionTitle main center>
+      Welcome To <br />
+      My Personal Portfolio
+    </SectionTitle>
+    <SectionText>
+      Fullstack Developer providing an end-to-end service, and involved in projects that engage with databases and building user-facing websites.
+    </SectionText>
+    <Button onClick={() => window.location = "https://google.com"}>Learn More</Button>     {/* more projects */}
+  </LeftSection>
+</Section>
 );
 
 export default Hero;
